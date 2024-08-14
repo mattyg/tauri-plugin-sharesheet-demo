11c0f05269e8026ea2cef759481d191f1ae3feb0
Matt Gabrenya <matt@buildyourweb.app>
2024-08-14 10:51:42 -0700

    basic sharesheet plugin demo

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { shareText } from "tauri-plugin-sharesheet";
+
+  let text = "This is shared text";
+  let title: string | undefined;
+
+  async function share() {
+    shareText(text, {title});
+  }
+</script>
+
+<div class="container">
+  <h1>Tauri Sharesheet Demo</h1>
+
+  <form class="row" on:submit|preventDefault={share}>
+    <div>Text: <input bind:value={text} /></div>
+    <div>Title: <input bind:value={title} /></div>
+
+    <button type="submit">Share</button>
+  </form>
+</div>
